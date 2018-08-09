8e8e1d345e14c4afcfb5932fe90ad45ca776d832
Oleksandr Andrieiev <kuricyak@gmail.com>
2018-08-09 14:16:20 +0300

    added blocking-event-loop

---
+++ blocking-event-loop.js
@@ -0,0 +1,20 @@
+// official solution
+function repeat(operation, num) { 
+  if (num <= 0) return;            
+                                  
+  operation()                     
+                                  
+  // release control every 10 or s
+  // iterations.                  
+  // 10 is arbitrary.             
+  if (num % 10 === 0) {           
+    setTimeout(function() {       
+      repeat(operation, --num);    
+    })                            
+  } else {                        
+    repeat(operation, --num) ;     
+  }                               
+}                                 
+                                  
+module.exports = repeat;          
+                                  
