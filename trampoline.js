30b6ff0891ed30751f8fad42675a1e1e02345c0b
Oleksandr Andrieiev <kuricyak@gmail.com>
2018-08-09 14:22:30 +0300

    added trampoline

---
+++ trampoline.js
@@ -0,0 +1,22 @@
+function repeat(operation, num) {
+    // Modify this so it doesn't cause a stack overflow!
+    if (num <= 0) return;
+    
+    return function() {
+      operation();
+      return repeat(operation, --num);
+    } 
+  }
+  
+  function trampoline(fn) {
+    if(fn && typeof fn === 'function')
+        fn = fn();
+    return fn;
+  }
+  
+  module.exports = function(operation, num) {
+    // You probably want to call your trampoline here!
+    return trampoline(function() {
+      return repeat(operation, num)
+    })
+  }
