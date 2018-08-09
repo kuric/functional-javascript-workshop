58e3679943d0d3cb34aae4596e3234f912b59170
Oleksandr Andrieiev <kuricyak@gmail.com>
2018-08-09 15:19:11 +0300

    added curriyng

---
+++ currying.js
@@ -0,0 +1,10 @@
+// official answer
+function curryN(fn, n) {
+    n = n || fn.length;
+    return function (arg) {
+        if(n <= 1) return fn(arg);
+        return curryN(fn.bind(this, arg) , n - 1);
+    }
+  }
+
+  module.exports = curryN;
